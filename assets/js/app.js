d9246721a6fa02fc0e34812fc035f93eda6205ca
Rafael Duarte <rf_duarte@outlook.com>
2022-11-21 10:34:27 -0300

    insert video

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,19 +5,19 @@ const navLogo = document.querySelector('.nav__logo')
 const goToTop = document.querySelector('.btn__top')
 
 function scrolled() {
-  if (window.pageYOffset > 0) {
-    goToTop.classList.add('active')
-    navBar.classList.add('active')
-    navLogo.innerHTML = ` 
+	if (window.pageYOffset > 0) {
+		goToTop.classList.add('active')
+		navBar.classList.add('active')
+		navLogo.innerHTML = ` 
              <a href="#home"><img src="./assets/images/logo.png" alt="" /></a>
     `
-  } else {
-    goToTop.classList.remove('active')
-    navBar.classList.remove('active')
-    navLogo.innerHTML = ` 
+	} else {
+		goToTop.classList.remove('active')
+		navBar.classList.remove('active')
+		navLogo.innerHTML = ` 
              <a href="#home"><img src="./assets/images/logoneg.png" alt="" /></a>
 `
-  }
+	}
 }
 
 scrolled()
@@ -27,79 +27,79 @@ window.addEventListener('scroll', scrolled)
 // Acordion
 const accordion = document.querySelectorAll('.accordion')
 accordion.forEach((target) => {
-  target.addEventListener('click', () => {
-    const itemInfo = target.querySelector('.accordion__info')
+	target.addEventListener('click', () => {
+		const itemInfo = target.querySelector('.accordion__info')
 
-    if (target.classList.contains('open')) {
-      target.classList.remove('open')
-      itemInfo.style.height = 0 + 'px'
-    } else {
-      target.classList.add('open')
-      itemInfo.style.height = itemInfo.scrollHeight + 'px'
-    }
-  })
+		if (target.classList.contains('open')) {
+			target.classList.remove('open')
+			itemInfo.style.height = 0 + 'px'
+		} else {
+			target.classList.add('open')
+			itemInfo.style.height = itemInfo.scrollHeight + 'px'
+		}
+	})
 })
 
 // ScrollReveal ===================
 const slideTop = {
-  mobile: true,
-  delay: 250,
-  duration: 750,
-  reset: true,
-  origin: 'top',
-  distance: '100px',
-  easing: 'cubic-bezier(.53,-0.23,.4,1.26)',
-  viewOffset: {
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0
-  }
+	mobile: true,
+	delay: 250,
+	duration: 750,
+	reset: true,
+	origin: 'top',
+	distance: '100px',
+	easing: 'cubic-bezier(.53,-0.23,.4,1.26)',
+	viewOffset: {
+		top: 0,
+		right: 0,
+		bottom: 0,
+		left: 0,
+	},
 }
 const slideBotton = {
-  mobile: true,
-  delay: 250,
-  duration: 750,
-  reset: true,
-  origin: 'bottom',
-  distance: '100px',
-  easing: 'cubic-bezier(.53,-0.23,.4,1.26)',
-  viewOffset: {
-    top: 100,
-    right: 0,
-    bottom: 0,
-    left: 0
-  }
+	mobile: true,
+	delay: 250,
+	duration: 750,
+	reset: true,
+	origin: 'bottom',
+	distance: '100px',
+	easing: 'cubic-bezier(.53,-0.23,.4,1.26)',
+	viewOffset: {
+		top: 100,
+		right: 0,
+		bottom: 0,
+		left: 0,
+	},
 }
 const slideleft = {
-  mobile: true,
-  delay: 250,
-  duration: 750,
-  reset: true,
-  origin: 'left',
-  distance: '100px',
-  easing: 'cubic-bezier(.53,-0.23,.4,1.26)',
-  viewOffset: {
-    top: 100,
-    right: 0,
-    bottom: 0,
-    left: 0
-  }
+	mobile: true,
+	delay: 250,
+	duration: 750,
+	reset: true,
+	origin: 'left',
+	distance: '100px',
+	easing: 'cubic-bezier(.53,-0.23,.4,1.26)',
+	viewOffset: {
+		top: 100,
+		right: 0,
+		bottom: 0,
+		left: 0,
+	},
 }
 const slideright = {
-  mobile: true,
-  delay: 250,
-  duration: 750,
-  reset: true,
-  origin: 'rigt',
-  distance: '100px',
-  easing: 'cubic-bezier(.53,-0.23,.4,1.26)',
-  viewOffset: {
-    top: 100,
-    right: 0,
-    bottom: 0,
-    left: 0
-  }
+	mobile: true,
+	delay: 250,
+	duration: 750,
+	reset: true,
+	origin: 'rigt',
+	distance: '100px',
+	easing: 'cubic-bezier(.53,-0.23,.4,1.26)',
+	viewOffset: {
+		top: 100,
+		right: 0,
+		bottom: 0,
+		left: 0,
+	},
 }
 
 ScrollReveal().reveal('#home .container', slideTop)
@@ -112,6 +112,7 @@ ScrollReveal().reveal('#services .services__container', slideBotton)
 ScrollReveal().reveal('#services .btn__dark', slideBotton)
 ScrollReveal().reveal('#contact .contact__info', slideleft)
 ScrollReveal().reveal('#contact .contact__form', slideright)
+ScrollReveal().reveal('#services iframe', slideTop)
 
 // Menu Mobile
 const menuMobileBtn = document.querySelector('.mobile__menu')
@@ -119,77 +120,77 @@ const menuLinks = document.querySelector('.nav__links')
 const links = document.querySelectorAll('.nav__links li ')
 
 menuMobileBtn.addEventListener('click', () => {
-  menuLinks.classList.toggle('active')
-  menuMobileBtn.classList.toggle('active')
+	menuLinks.classList.toggle('active')
+	menuMobileBtn.classList.toggle('active')
 })
 
 links.forEach((link) => {
-  link.addEventListener('click', () => {
-    menuLinks.classList.remove('active')
-    menuMobileBtn.classList.remove('active')
-  })
+	link.addEventListener('click', () => {
+		menuLinks.classList.remove('active')
+		menuMobileBtn.classList.remove('active')
+	})
 })
 
 // Form request
 const form = document.querySelector('.contact__form form')
 
 async function sendForm(nome, escola, email, msg, form) {
-  await fetch('https://formsubmit.co/ajax/fabiofpauli@gmail.com', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json'
-    },
-    body: JSON.stringify({
-      nome: nome,
-      escola: escola,
-      email: email,
-      mensagem: msg
-    })
-  })
-    .then(() => {
-      console.log('teste')
-      nome.innerHTML = ''
-      escola.innerHTML = ''
-      email.innerHTML = ''
-      msg.innerHTML = ''
-      form.classList.remove('load')
-    })
-    .catch((error) => console.log(error))
+	await fetch('https://formsubmit.co/ajax/fabiofpauli@gmail.com', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json',
+		},
+		body: JSON.stringify({
+			nome: nome,
+			escola: escola,
+			email: email,
+			mensagem: msg,
+		}),
+	})
+		.then(() => {
+			console.log('teste')
+			nome.innerHTML = ''
+			escola.innerHTML = ''
+			email.innerHTML = ''
+			msg.innerHTML = ''
+			form.classList.remove('load')
+		})
+		.catch((error) => console.log(error))
 }
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const loadForm = document.querySelector('.form__load')
-  const formName = document.querySelector('#form__name')
-  const formSchool = document.querySelector('#form__school')
-  const formEmail = document.querySelector('#form__email')
-  const formMsg = document.querySelector('#form__msg')
+	e.preventDefault()
+	const loadForm = document.querySelector('.form__load')
+	const formName = document.querySelector('#form__name')
+	const formSchool = document.querySelector('#form__school')
+	const formEmail = document.querySelector('#form__email')
+	const formMsg = document.querySelector('#form__msg')
 
-  loadForm.classList.add('load')
-  
-  try {
-    fetch('https://formsubmit.co/ajax/960428ad7b9c5966de6a6e5d704c4623', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json'
-      },
-      body: JSON.stringify({
-        nome: formName,
-        escola: formSchool,
-        email: formEmail,
-        mensagem: formMsg
-      })
-    }).then(() => {
-      console.log('mensagem enviada')
-      formName.value = ''
-      formSchool.value = ''
-      formEmail.value = ''
-      formMsg.value = ''
-      loadForm.classList.remove('load')
-    })
-  } catch {
-    ;(error) => console.log(error)
-  }
+	loadForm.classList.add('load')
+
+	try {
+		fetch('https://formsubmit.co/ajax/960428ad7b9c5966de6a6e5d704c4623', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+			body: JSON.stringify({
+				nome: formName,
+				escola: formSchool,
+				email: formEmail,
+				mensagem: formMsg,
+			}),
+		}).then(() => {
+			console.log('mensagem enviada')
+			formName.value = ''
+			formSchool.value = ''
+			formEmail.value = ''
+			formMsg.value = ''
+			loadForm.classList.remove('load')
+		})
+	} catch {
+		;(error) => console.log(error)
+	}
 })
